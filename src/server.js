66f9cac3b64059850dba996813267508038cc119
Adriano <carlosadrianosmoreira@gmail.com>
2020-05-30 09:50:13 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import 'dotenv/config';
 import express from './index';
 
-const port = process.env.PORT || 3333;
-express.listen(port, () => console.log('Api rodando na porta : ' + port));
+express.listen(process.env.PORT || 3333);
